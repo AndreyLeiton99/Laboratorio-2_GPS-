5e2ab80bba1641493573521ba3bb7dd4d363dc46
AndreyLeiton99 <aleiton2014@hotmail.com>
2023-04-18 00:12:57 -0600

    Correction Commit
    Uploaded just folder's name, not the content

---
+++ app/src/main/java/cr/ac/menufragment/HomeFragment.kt
@@ -0,0 +1,65 @@
+package cr.ac.menufragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private lateinit var textInput: EditText
+    private lateinit var button: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        textInput = view.findViewById(R.id.markerText)
+        button = view.findViewById<Button>(R.id.botonCambiar)
+
+        button.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("Etiqueta", textInput.text.toString())
+            val mapsFragment = MapsFragment()
+            mapsFragment.arguments = bundle
+            val fragmentManager = requireActivity().supportFragmentManager
+            fragmentManager.beginTransaction().replace(R.id.home_content, mapsFragment).commit()
+        }
+        return view
+    }
+
+    companion object {
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                }
+            }
+    }
+}
